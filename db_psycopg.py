0ab290247de161a1601935d6ba756c7d2b840184
Javohir <javoxirone@gmail.com>
2023-11-15 12:45:30 +0500

    setup postgres, fixed bugs, filled out the database with records of users

--- db_psycopg.py
+++ db_psycopg.py
@@ -1,15 +1,17 @@
+import os
+
 import psycopg2
 
 
 class Database:
     def __init__(self, db_name=None):
-        self.db_name = db_name or "bilagon"
+        self.db_name = db_name or os.getenv("DB_NAME")
         self.conn = psycopg2.connect(
             database=self.db_name,
-            user="postgres",
-            password="123456",
-            host="localhost",
-            port="5432"
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
         )
         self.create_table()
 
